b2da5d07bd746ce1902f71a031c2f8d179f6f6b3
mb-zaba <mathis@bonnin.dev>
2021-02-20 12:21:30 +0100

    Update krakenpy.py

--- krakenpy.py
+++ krakenpy.py
@@ -2,7 +2,7 @@
 # @Author: mb-zaba
 # @Date:   2021-02-19 15:54:59
 # @Last Modified by:   mb-zaba
-# @Last Modified time: 2021-02-20 11:05:27
+# @Last Modified time: 2021-02-20 12:18:35
 
 import pandas as pd
 import requests
@@ -14,121 +14,182 @@ class krakenpy:
 	def __init__(self, api_key=None):
 		self.api_url = "https://api.kraken.com/0"
 		self.api_key = api_key
+		self.public = self.public(self.api_url)
 
 
+	
+	class public:
 
-	# Gets server time from the Kraken API
-	def get_server_time(self):
-		endpoint = f"{self.api_url}/public/Time"
-		r = requests.get(endpoint)
-		data = r.json()
-		if len(data['error']) < 1:
-			server_time = data['result']['rfc1123']
-			return server_time
-		else:
-			print(f"Error: {data['error']}")
+		def __init__(self, api_url):
+			self.api_url = api_url
 
 
-
-	# Gets informations about the specified assets (all if none is specified)
-	def get_asset_info(self, assets=None):
-		endpoint = f"{self.api_url}/public/Assets"
-		if assets == None:
+		# Gets server time from the Kraken API
+		def get_server_time(self):
+			endpoint = f"{self.api_url}/public/Time"
 			r = requests.get(endpoint)
-		else:
-			data = {"":",".join(assets)}
-			r = requests.post(endpoint, data=data)
-		data = r.json()
-		if len(data['error']) < 1:
-			#pd.set_option('display.max_rows', None)
-			#assets = pd.DataFrame(data['result'])
-			assets = data['result']
-			return assets
-		else:
-			print(f"Error: {data['error']}")
-
-
-
-	# Gets informations about an asset pair
-	def get_asset_pairs(self, pairs=None):
-		endpoint = f"{self.api_url}/public/AssetPairs"
-		if pairs == None:
-			r = requests.get(endpoint)
-		else:
+			data = r.json()
+			if len(data['error']) < 1:
+				server_time = data['result']['rfc1123']
+				return server_time
+			else:
+				print(f"Error: {data['error']}")
+	
+	
+	
+		# Gets informations about the specified assets (all if none is specified)
+		def get_asset_info(self, assets=None):
+			endpoint = f"{self.api_url}/public/Assets"
+			if assets == None:
+				r = requests.get(endpoint)
+			else:
+				data = {"":",".join(assets)}
+				r = requests.post(endpoint, data=data)
+			data = r.json()
+			if len(data['error']) < 1:
+				#pd.set_option('display.max_rows', None)
+				#assets = pd.DataFrame(data['result'])
+				assets = data['result']
+				return assets
+			else:
+				print(f"Error: {data['error']}")
+	
+	
+	
+		# Gets informations about an asset pair
+		def get_asset_pairs(self, pairs=None):
+			endpoint = f"{self.api_url}/public/AssetPairs"
+			if pairs == None:
+				r = requests.get(endpoint)
+			else:
+				r = requests.post(endpoint, data={'pair':",".join(pairs)})
+			data = r.json()
+			if len(data['error']) < 1:
+				pd.set_option('display.max_rows', None)
+				assets = pd.DataFrame(data['result'])
+				return assets
+			else:
+				print(f"Error: {data['error']}")
+	
+	
+	
+		# Get Ticker
+		def get_ticker(self, pairs):
+			endpoint = f"{self.api_url}/public/Ticker"
 			r = requests.post(endpoint, data={'pair':",".join(pairs)})
-		data = r.json()
-		if len(data['error']) < 1:
-			pd.set_option('display.max_rows', None)
-			assets = pd.DataFrame(data['result'])
-			return assets
-		else:
-			print(f"Error: {data['error']}")
+			data = r.json()
+			if len(data['error']) < 1:
+				#pd.set_option('display.max_rows', None)
+				#assets = pd.DataFrame(data['result'])
+				assets = data['result']
+				return assets
+			else:
+				print(f"Error: {data['error']}")
+	
+	
+	
+		# Get OHLC
+		def get_ohlc(self, pair):
+			endpoint = f"{self.api_url}/public/OHLC"
+			r = requests.post(endpoint, data={'pair':pair})
+			data = r.json()
+			data_struct = {
+				"time": [],
+				"open": [],
+				"high": [],
+				"low": [],
+				"close": [],
+				"vwap": [],
+				"volume": [],
+				"count": []
+			}
+			if len(data['error']) < 1:
+				for ohlc_data in data['result']:
+					if type(data['result'][ohlc_data]) == type([]):
+						ohlc_data = data['result'][ohlc_data]
+						break
+	
+				for row in ohlc_data:
+					i = 0
+					for entry in data_struct:
+						data_struct[entry].append(row[i])
+						i += 1
+				data_struct['pair'] = pair
+				#df = pd.DataFrame(data_struct)
+				return data_struct
+			else:
+				print(f"Error: {data['error']}")
+	
+	
+	
+		def get_order_book(self, pair):
+			endpoint = f"{self.api_url}/public/Depth"
+			r = requests.post(endpoint, data={'pair':pair})
+			data = r.json()
+			if len(data['error']) < 1:
+				return data['result']
+			else:
+				print(f"Error: {data['error']}")
+	
+	
+	
+		def get_recent_trades(self, pair):
+			endpoint = f"{self.api_url}/public/Trades"
+			r = requests.post(endpoint, data={'pair':pair})
+			data = r.json()
+			data_struct = {
+				"price": [],
+				"volume": [],
+				"time": [],
+				"buy/sell": [],
+				"market/limit": [],
+				"miscellaneous": []
+			}
+			if len(data['error']) < 1:
+				if len(data['error']) < 1:
+					for trade_data in data['result']:
+						if type(data['result'][trade_data]) == type([]):
+							trade_data = data['result'][trade_data]
+							break
+	
+					for row in trade_data:
+						i = 0
+						for entry in data_struct:
+							data_struct[entry].append(row[i])
+							i += 1
+	
+				data_struct['pair'] = pair
+				#data_struct = pd.DataFrame(data_struct)
+				return data_struct
+			else:
+				print(f"Error: {data['error']}")
+	
+		
+		def get_recent_spread(self, pair):
+			endpoint = f"{self.api_url}/public/Spread"
+			r = requests.post(endpoint, data={'pair':pair})
+			data = r.json()
+			data_struct = {
+				"time": [],
+				"bid": [],
+				"ask": []
+			}
+			if len(data['error']) < 1:
+				if len(data['error']) < 1:
+					for spread_data in data['result']:
+						if type(data['result'][spread_data]) == type([]):
+							spread_data = data['result'][spread_data]
+							break
+	
+					for row in spread_data:
+						i = 0
+						for entry in data_struct:
+							data_struct[entry].append(row[i])
+							i += 1
+	
+				data_struct['pair'] = pair
+				#data_struct = pd.DataFrame(data_struct)
+				return data_struct
+			else:
+				print(f"Error: {data['error']}")
 
-
-
-	# Get Ticker
-	def get_ticker(self, pairs):
-		endpoint = f"{self.api_url}/public/Ticker"
-		r = requests.post(endpoint, data={'pair':",".join(pairs)})
-		data = r.json()
-		if len(data['error']) < 1:
-			#pd.set_option('display.max_rows', None)
-			#assets = pd.DataFrame(data['result'])
-			assets = data['result']
-			return assets
-		else:
-			print(f"Error: {data['error']}")
-
-
-
-	# Get OHLC
-	def get_ohlc(self, pair):
-		endpoint = f"{self.api_url}/public/OHLC"
-		data_struct = {
-			"time": [],
-			"open": [],
-			"high": [],
-			"low": [],
-			"close": [],
-			"vwap": [],
-			"volume": [],
-			"count": []
-		}
-		r = requests.post(endpoint, data={'pair':pair})
-		data = r.json()
-		if len(data['error']) < 1:
-			for ohlc_data in data['result']:
-				if type(data['result'][ohlc_data]) == type([]):
-					ohlc_data = data['result'][ohlc_data]
-					break
-
-			for row in ohlc_data:
-				i = 0
-				for entry in data_struct:
-					data_struct[entry].append(row[i])
-					i += 1
-			data_struct['pair'] = pair
-			#df = pd.DataFrame(data_struct)
-			return data_struct
-		else:
-			print(f"Error: {data['error']}")
-
-
-
-	def get_order_book(self, pair):
-		endpoint = f"{self.api_url}/public/Depth"
-		r = requests.post(endpoint, data={'pair':pair})
-		data = r.json()
-		if len(data['error']) < 1:
-			return data['result']
-
-
-
-
-if __name__ == '__main__':
-	k = krakenpy()
-	#k.get_server_time()
-	#k.get_asset_info()
-	#k.get_asset_pairs()
-	#k.get_ticker(['ETHEUR', 'XDGEUR'])
-	#print(k.get_ohlc('ETHEUR'))
