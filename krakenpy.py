8ce4d033cd69e87609c3e4dea66ce7d4ff94981f
mb-zaba <mathis@bonnin.dev>
2021-07-14 13:59:54 +0200

    Added Private class

--- krakenpy.py
+++ krakenpy.py
@@ -1,23 +1,30 @@
 # -*- coding: utf-8 -*-
 # @Author: mb-zaba
 # @Date:   2021-02-19 15:54:59
-# @Last Modified by:   mb-zaba
-# @Last Modified time: 2021-03-01 17:06:21
+# @Last modified by:   zaba
+# @Last modified time: 2021-07-14T13:57:52+02:00
+# @Version: 1.0
 
 import pandas as pd
 import requests
 from dotenv import load_dotenv
+import time
+import urllib.parse
+import hashlib
+import hmac
+import base64
 
 class Krakenpy:
 
-
-	def __init__(self, api_key=None):
+	def __init__(self, api_key=None, api_sec=None):
 		self.api_url = "https://api.kraken.com/0"
 		self.api_key = api_key
+		self.api_sec = api_sec
 		self.public = self.Public(self.api_url)
+		self.private = self.Private(self.api_key, self.api_url, self.api_sec)
 
 
-	
+
 	class Public:
 
 		def __init__(self, api_url):
@@ -34,9 +41,9 @@ class Krakenpy:
 				return server_time
 			else:
 				print(f"Error: {data['error']}")
-	
-	
-	
+
+
+
 		# Gets informations about the specified assets (all if none is specified)
 		def get_asset_info(self, assets=None):
 			endpoint = f"{self.api_url}/public/Assets"
@@ -53,9 +60,9 @@ class Krakenpy:
 				return assets
 			else:
 				print(f"Error: {data['error']}")
-	
-	
-	
+
+
+
 		# Gets informations about an asset pair
 		def get_asset_pairs(self, pairs=None):
 			endpoint = f"{self.api_url}/public/AssetPairs"
@@ -70,9 +77,9 @@ class Krakenpy:
 				return assets
 			else:
 				print(f"Error: {data['error']}")
-	
-	
-	
+
+
+
 		# Get Ticker
 		def get_ticker(self, pairs):
 			endpoint = f"{self.api_url}/public/Ticker"
@@ -85,9 +92,9 @@ class Krakenpy:
 				return assets
 			else:
 				print(f"Error: {data['error']}")
-	
-	
-	
+
+
+
 		# Get OHLC
 		def get_ohlc(self, pair, interval=1, since=None):
 			endpoint = f"{self.api_url}/public/OHLC"
@@ -111,7 +118,7 @@ class Krakenpy:
 					if type(data['result'][ohlc_data]) == type([]):
 						ohlc_data = data['result'][ohlc_data]
 						break
-	
+
 				for row in ohlc_data:
 					i = 0
 					for entry in data_struct:
@@ -122,9 +129,9 @@ class Krakenpy:
 				return data_struct
 			else:
 				print(f"Error: {data['error']}")
-	
-	
-	
+
+
+
 		def get_order_book(self, pair):
 			endpoint = f"{self.api_url}/public/Depth"
 			r = requests.post(endpoint, data={'pair':pair})
@@ -133,9 +140,9 @@ class Krakenpy:
 				return data['result']
 			else:
 				print(f"Error: {data['error']}")
-	
-	
-	
+
+
+
 		def get_recent_trades(self, pair):
 			endpoint = f"{self.api_url}/public/Trades"
 			r = requests.post(endpoint, data={'pair':pair})
@@ -154,20 +161,20 @@ class Krakenpy:
 						if type(data['result'][trade_data]) == type([]):
 							trade_data = data['result'][trade_data]
 							break
-	
+
 					for row in trade_data:
 						i = 0
 						for entry in data_struct:
 							data_struct[entry].append(row[i])
 							i += 1
-	
+
 				data_struct['pair'] = pair
 				#data_struct = pd.DataFrame(data_struct)
 				return data_struct
 			else:
 				print(f"Error: {data['error']}")
-	
-		
+
+
 		def get_recent_spread(self, pair):
 			endpoint = f"{self.api_url}/public/Spread"
 			r = requests.post(endpoint, data={'pair':pair})
@@ -183,16 +190,51 @@ class Krakenpy:
 						if type(data['result'][spread_data]) == type([]):
 							spread_data = data['result'][spread_data]
 							break
-	
+
 					for row in spread_data:
 						i = 0
 						for entry in data_struct:
 							data_struct[entry].append(row[i])
 							i += 1
-	
+
 				data_struct['pair'] = pair
-				#data_struct = pd.DataFrame(data_struct)
 				return data_struct
 			else:
 				print(f"Error: {data['error']}")
 
+
+
+	class Private:
+
+		def __init__(self, api_key, api_url, api_sec):
+			self.api_key = api_key
+			self.api_sec = api_sec
+			self.api_url = api_url
+
+
+
+		def get_api_sign(self, URI_Path, payload):
+			# HMAC-SH512(URI_Path + SHA256(nonce + POST data)) and base64 decoded secret API key
+			postdata = urllib.parse.urlencode(payload)
+			encoded = (str(payload["nonce"]) + postdata).encode()
+			hashed_payload = URI_Path.encode() + hashlib.sha256(encoded).digest()
+
+			api_sign = base64.b64encode(hmac.new(base64.b64decode(self.api_sec), hashed_payload, hashlib.sha512).digest())
+			return api_sign
+
+
+		def get_account_balance(self):
+			data = {
+			"nonce": str(int(time.time()*1000))
+			}
+			headers = {}
+			headers["API-Sign"] = self.get_api_sign("/0/private/Balance", data)
+			headers["API-Key"] = self.api_key
+			endpoint = f"{self.api_url}/private/Balance"
+			r = requests.post(endpoint, headers=headers, data=data)
+			data = r.json()
+			if len(data['error']) < 1:
+				if len(data['error']) < 1:
+					return data["result"]
+			else:
+				print(f"Error: {data['error']}")
